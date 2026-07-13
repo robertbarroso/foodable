cd9f73f6b858ff336af77095e8bbb9e4fa31798b
robertbarroso <r0230323@ucf.edu>
2026-07-13 16:33:22 -0400

    CHANGED: Changed how the text in the community field is rendered, added a (not working) like button. Changed design to match Lists

--- foodable/src/pages/SocialFeed.jsx
+++ foodable/src/pages/SocialFeed.jsx
@@ -3,41 +3,72 @@ import { supabase } from "../services/supabase.js";
 
 function SocialFeed() {
   const [posts, setPosts] = useState([]);
+  const [selected_post, setSelectedPost] = useState(null);
 
+  // When the page refreshes
   useEffect(() => {
     const fetchPosts = async () => {
-      const { data, error } = await supabase.from("posts").select("*");
+      const { data, error } = await supabase.from("posts").select(`
+    *,
+    author:profiles!posts_user_id_fkey (
+      username
+    )
+  `);
 
+      // If something goes wrong, send error
       if (error) {
         console.error(error);
         return;
       }
 
       setPosts(data);
+      console.log(JSON.stringify(data, null, 2));
     };
 
     fetchPosts();
   }, []);
 
+  function loadSelectedContent() {
+    return selected_post ? (
+      <div className="content-post-render">
+        <h2>{selected_post.title} </h2>
+        <br></br>
+        <p>{selected_post.content}</p>
+      </div>
+    ) : (
+      <div className="placeholder-social-post">
+        <h3>Select a post</h3>
+        <p>To see shared community posts, select a post.</p>
+      </div>
+    );
+  }
+
   return (
     <>
-      <h1>Community</h1>
       <section className="content-container">
         <section id="post-list">
-          <h2>Posts</h2>
+          <h2>Community</h2>
           {posts.map((post) => {
             return (
-              <div key={post.id}>
-                <h3>{post.title} </h3>
-                <p>{post.content}</p>
-                <p> Likes: {post.likes} </p>
+              <div key={post.post_id}>
+                <button
+                  onClick={() => {
+                    setSelectedPost(post);
+                  }}
+                  className="social-button-posts"
+                >
+                  <h4>{post.title} </h4>
+                  <p className="post-author-render">
+                    By {post.author?.username}
+                  </p>
+
+                  <p className="likes-render"> ♥︎ {post.likes} </p>
+                </button>
               </div>
             );
           })}
         </section>
-        <section id="content-view">
-          <h2>Content</h2>
-        </section>
+        <section id="content-view">{loadSelectedContent()}</section>
       </section>
     </>
   );
