571f8662756604ba1ee477cfd0f25b8b032bb24c
robertbarroso <r0230323@ucf.edu>
2026-08-02 15:56:41 -0400

    ADDED: Started full text rendering for recipe and grocery posts. Now titles and author information (plus date and likes) are viewable

--- foodable/src/components/GroceryPost.jsx
+++ foodable/src/components/GroceryPost.jsx
@@ -4,6 +4,30 @@ export default function GroceryPost({ grocery_post }) {
     console.error("ERROR: No grocery post has been found!", grocery_post);
     return <p>No grocery post received.</p>;
   }
-
-  return <pre>{JSON.stringify(grocery_post, null, 2)}</pre>;
+  const groceryCreated = new Date(grocery_post.created_date).toLocaleDateString(
+    "en-US",
+    {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    },
+  );
+  return (
+    <>
+      <article grocery-post-header>
+        <h3 id="post-title">{grocery_post.grocery.title}</h3>
+        <div className="post-information-full">
+          <p className="pill-render-post likes-render post-information-content">
+            ♥ {grocery_post.likes}
+          </p>
+          <p className="post-information-content">
+            Created by <b>{grocery_post.profiles.username}</b>
+          </p>
+          <p className="post-information-content">
+            Posted on: <b>{groceryCreated}</b>
+          </p>
+        </div>
+      </article>
+    </>
+  );
 }
--- foodable/src/components/RecipePost.jsx
+++ foodable/src/components/RecipePost.jsx
@@ -3,5 +3,34 @@ export default function RecipePost({ recipe_post }) {
     console.error("ERROR: No recipe post has been found!", recipe_post);
     return <p>No recipe post received.</p>;
   }
-  return <pre>{JSON.stringify(recipe_post, null, 2)}</pre>;
+
+  console.log(recipe_post);
+
+  const recipeCreated = new Date(recipe_post.created_date).toLocaleDateString(
+    "en-US",
+    {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    },
+  );
+  return (
+    <>
+      <article recipe-post-header>
+        <h3 id="post-title">{recipe_post.recipe.title}</h3>
+        <section className="post-information-full">
+          <p className="pill-render-post likes-render post-information-content">
+            ♥ {recipe_post.likes}
+          </p>
+          <p className="post-information-content">
+            Created by <b>{recipe_post.profiles.username}</b>
+          </p>
+          <p className="post-information-content">
+            Posted on <b>{recipeCreated}</b>
+          </p>
+        </section>
+        <section id="recipe-macros"></section>
+      </article>
+    </>
+  );
 }
